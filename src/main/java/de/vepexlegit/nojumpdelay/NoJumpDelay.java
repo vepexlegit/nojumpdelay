043a3b26e87d6c71203b32b8c7820ff8ee09b3ff
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-06 13:23:34 +0400

    Create NoJumpDelay.java

---
+++ src/main/java/de/vepexlegit/nojumpdelay/NoJumpDelay.java
@@ -0,0 +1,13 @@
+package de.vepexlegit.nojumpdelay;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = NoJumpDelay.MOD_ID)
+public class NoJumpDelay {
+    public static final String MOD_ID = "nojumpdelay";
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+    }
+}
