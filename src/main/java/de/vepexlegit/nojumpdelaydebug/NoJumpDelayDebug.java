dfcd6dceb4ab7771973616c63aa476d4b29c8502
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-24 20:20:32 +0400

    Create NoJumpDelayDebug.java

---
+++ src/main/java/de/vepexlegit/nojumpdelaydebug/NoJumpDelayDebug.java
@@ -0,0 +1,13 @@
+package de.vepexlegit.nojumpdelaydebug;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = NoJumpDelayDebug.MOD_ID)
+public class NoJumpDelayDebug {
+    public static final String MOD_ID = "nojumpdelaydebug";
+
+    @Mod.EventHandler
+    public void onPreInit(FMLPreInitializationEvent event) {
+    }
+}
